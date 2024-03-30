1cca4e448c9cb687bd8c377edbdcd2129fe880c4
shivamvarshneyy <shivamvarshney1506@gmail.com>
2024-03-30 09:30:48 +0530

    commit

---
+++ Tree-2/Day2/PathSumIII.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int dfs(TreeNode* root, int sum) {
+        if (!root) 
+        return 0;
+        return (root->val == sum) + dfs(root->left, sum - root->val) + dfs(root->right, sum - root->val);
+    }
+public:
+    int pathSum(TreeNode* root, int sum) {
+        if (!root) 
+        return 0;
+        return dfs(root, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
+    }
+
+    
+};
